eac26fadc622b24736388e9d068e48b8e8750320
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2022-03-18 20:06:40 -0300

    Add files via upload

---
+++ variaveis operadoresaritimeticos/media.lua
@@ -0,0 +1,41 @@
+Algoritmo "CalcularMedia"
+// Disciplina : [Linguagem e Lógica de Programação]
+// Professora : Cintia Pinho
+// Descrição : Calcular a media de 3 notas
+// Autor(a): sarah 
+// Dados atuais: 08/03/2022
+Var
+n1,n2,n3,media:real
+
+Início
+//entrada
+escreval("--------------------------")
+escreval("Digite primeiro numero",n1)
+escreval("--------------------------")
+leia(n1)
+escreval("--------------------------")
+escreval("Digite segundo numero",n2)
+escreval("--------------------------")
+leia(n2)
+escreval("--------------------------")
+escreval("Digite terceiro numero",n3)
+escreval("--------------------------")
+leia(n3)
+
+//processamento
+mídia <- (n1+n2+n3)/3
+limpatela
+
+//saida
+escreval ("--------------------")
+escreval("A media final é:" ,media)
+escreval ("--------------------")
+
+
+
+
+
+
+
+
+Fimalgoritmo
